93bf25924635652fb141f7c0a8f189933992795f
{Jashandeep <{jashandeep20445@gmail.com}>
2025-12-23 22:08:38 +0530

    Fixing MIME type and path rules

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths to find assets correctly
+  base: '/', 
   build: {
     outDir: 'dist'
   }
